5f17625a6eff224474b3b807a4f90759f5b1ab82
PETREMANN Marc <57889978+MPETREMANN11@users.noreply.github.com>
2024-10-27 11:00:16 +0100

    Add files via upload

--- SDL2/SDL2.fs
+++ SDL2/SDL2.fs
@@ -17,7 +17,7 @@ z" SDL2.dll" dll SDL2
 SDL2
 
 \ load SDL2 library
-s" windowsFlags.fs" included
+s" SDLconstants.fs" included
 
 
 
@@ -25,7 +25,7 @@ s" windowsFlags.fs" included
 
 
 
-\ compute arc cosine of x                               @ERROR: dont- work
+\ compute arc cosine of x                                       @ERROR: dont- work
 \ z" SDL_acos"                1 SDL2 acos ( float:n -- n ) 
 
 
@@ -34,14 +34,17 @@ s" windowsFlags.fs" included
 
 \ infos: https://wiki.libsdl.org/SDL2/SDL_CreateRenderer
 \ Create a 2D rendering context for a window
-z" SDL_CreateRenderer"      3 SDL2 CreateRenderer (  -- ) 
+z" SDL_CreateRenderer"      3 SDL2 CreateRenderer (  -- render ) 
 
 \ infos: https://wiki.libsdl.org/SDL2/SDL_CreateWindow
 \ Create a window with the specified position, dimensions, and flags.
 z" SDL_CreateWindow"        6 SDL2 CreateWindow ( strz x y w h fl -- win ) 
 
+\ Destroy the rendering context for a window and free associated textures
+z" SDL_DestroyRenderer"     1 SDL2 DestroyRenderer ( render -- ) 
 
-
+\ Destroy a window.
+z" SDL_DestroyWindow"       1 SDL2 DestroyWindow ( window -- ) 
 
 \ returns the SDL_AudioStatus of the audio device opened by SDL_OpenAudio
 z" SDL_GetAudioStatus"      0 SDL2 GetAudioStatus ( -- status ) 
@@ -58,16 +61,36 @@ z" SDL_GetCursor"           0 SDL2 GetCursor ( -- n )
 \ Retrieve a message about the last error that occurred on the current thread
 z" SDL_GetError"            1 SDL2 GetError ( n -- ) 
 
-
-
-
-
-
 \ infos: https://wiki.libsdl.org/SDL2/SDL_Init
 \ Initialize the SDL library
 \ Returns 0 on success or a negative error code on failure; 
 \ call SDL_GetError() for more information
-z" SDL_Init"                1 SDL2 Init ( n -- n ) 
+z" SDL_Init"                1 SDL2 Init ( n -- 0|err ) 
+
+
+\ \ structure that defines a point                        @ERROR: don-t work ??
+\ \ z" SDL_Point"               0 SDL2 Point ( x y -- ) 
+
+
+\ Poll for currently pending events.
+\ Returns 1 if there is a pending event or 0 if there are none available.
+z" SDL_PollEvent"           0 SDL2 PollEvent ( -- n ) 
+
+\ Clear the current rendering target with the drawing color
+z" SDL_RenderClear"                1 SDL2 RenderClear ( render -- 0|err ) 
+
+\ Update the screen with any rendering performed since the previous call
+z" SDL_RenderPresent"       1 SDL2 RenderPresent ( render -- ) 
+
+\ Clean up all initialized subsystems.
+z" SDL_Quit"                0 SDL2 Quit ( win -- ) 
+
+\ Set the color used for drawing operations (Rect, Line and Clear)
+z" SDL_SetRenderDrawColor"  5 SDL2 SetRenderDrawColor ( renderer r g b a -- fl ) 
+
+
+
+\ ***  Upper level words definitions  ******************************************
 
 \ Initialize SDL with error management
 : SDL.init ( n -- )
@@ -77,10 +100,12 @@ z" SDL_Init"                1 SDL2 Init ( n -- n )
     then
   ;
 
+\ Create a window with the specified position, dimensions, and flags.
+: SDL.CreateWindow  ( strz x y w h fl -- win )
+    CreateWindow ?dup 0=
+    if
+        ." Window could not be created! SDL_Error: " getError .
+    then
+  ;
 
-
-
-
-\ structure that defines a point                        @ERROR: don-t work ??
-\ z" SDL_Point"               0 SDL2 Point ( x y -- ) 
 
--- SDL2/tests.fs
+++ SDL2/tests.fs
@@ -2,12 +2,16 @@
 \ SDL2 / tests
 \    Filename:      tests.fs
 \    Date:          24 oct 2024
-\    Updated:       24 oct 2024
+\    Updated:       26 oct 2024
 \    File Version:  1.0
 \    Forth:         eFORTH Windows
 \    Author:        Marc PETREMANN
 \    GNU General Public License
 \ *********************************************************************
+
+
+forth definitions
+SDL2
 
 \ initialize SDL environment
 SDL_INIT_VIDEO SDL.Init
@@ -18,14 +22,43 @@ SDL_INIT_VIDEO SDL.Init
 200 constant X0_SCREEN_POSITION
  50 constant Y0_SCREEN_POSITION
 
-z" My first window with SDL2" 
-    X0_SCREEN_POSITION Y0_SCREEN_POSITION 
-    SCREEN_WIDTH SCREEN_HEIGHT 
-    SDL_WINDOW_SHOWN CreateWindow
-    value WIN0
+0 value WIN0
+0 value REN0
 
-\ WIN0 -1 0 CreateRenderer
-\ value REN0
+: initNewWindow ( -- )
+    \ create new window
+    z" My first window with SDL2"    
+    X0_SCREEN_POSITION Y0_SCREEN_POSITION
+    SCREEN_WIDTH SCREEN_HEIGHT
+    SDL_WINDOW_SHOWN
+    SDL.CreateWindow  to WIN0
+    \ create a new renderer
+    WIN0 -1 0 CreateRenderer  to REN0
+  ;
 
+variable SDLquit
+variable SDLevent
 
+: draw ( -- )
+    REN0 255 0 0 255 SetRenderDrawColor drop
+    REN0 RenderClear drop
+  ;
+
+\ free ressources, end renderer and window
+: freeRessources ( -- )
+    REN0 DestroyRenderer drop
+    WIN0 DestroyWindow   drop
+    Quit
+  ;
+
+: main ( -- )
+    initNewWindow
+    draw
+    begin
+        key drop
+    -1 until
+    freeRessources
+  ;
+
+\ main
 
